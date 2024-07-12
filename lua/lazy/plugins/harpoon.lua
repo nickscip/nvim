3823d489cfa0553a3d26384d90ac85230353786b
Nickolas Scipione <88822637+nickscip@users.noreply.github.com>
2024-07-12 14:46:37 -0500

    Replaced harpoon native ui with telescope ui

--- lua/lazy/plugins/harpoon.lua
+++ lua/lazy/plugins/harpoon.lua
@@ -7,12 +7,29 @@ return {
         config = function()
                 local harpoon = require("harpoon")
 
-                harpoon:setup()
+                harpoon:setup({})
+
+                local conf = require("telescope.config").values
+                local function toggle_telescope(harpoon_files)
+                    local file_paths = {}
+                    for _, item in ipairs(harpoon_files.items) do
+                        table.insert(file_paths, item.value)
+                    end
+
+                    require("telescope.pickers").new({}, {
+                        prompt_title = "Harpoon",
+                        finder = require("telescope.finders").new_table({
+                            results = file_paths,
+                        }),
+                        previewer = conf.file_previewer({}),
+                        sorter = conf.generic_sorter({}),
+                    }):find()
+                end
 
                 local keymap = vim.keymap
 
                 keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-                keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+                keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end)
 
                 keymap.set("n", "<leader>hq", function() harpoon:list():select(1) end)
                 keymap.set("n", "<leader>hw", function() harpoon:list():select(2) end)
