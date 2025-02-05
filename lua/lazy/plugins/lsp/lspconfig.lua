a2359ba2a1995067ab857f98559ccc4137189aec
nickscip <nickscip@gmail.com>
2025-02-05 12:35:15 -0600

    fix: remove optional opt that was throwing an LspAttach error

--- lua/lazy/plugins/lsp/lspconfig.lua
+++ lua/lazy/plugins/lsp/lspconfig.lua
@@ -24,7 +24,7 @@ return {
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if not client then return end
 
-        if client.supports_method('textDocument/formatting', 0) then
+        if client.supports_method('textDocument/formatting') then
           vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = args.buf,
             callback = function()
